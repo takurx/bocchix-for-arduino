bd0bf744699d52a60602d219de8be32873e7c45a
takurx <taku3wzr@gmail.com>
2015-10-03 14:26:29 +0900

    add to can off, maybe
    maybe can on/off on LED

--- bocchix_sketch/bocchix_sketch.ino
+++ bocchix_sketch/bocchix_sketch.ino
@@ -1,54 +1,100 @@
+//Bocchix Code for arduino
+//Connected konashi and iPhone, 
+//Bocchix Code - jsdo.it - Share JavaScript, HTML5 and CSS, http://jsdo.it/tezsawa/jtMj
+
 #include <Adafruit_NeoPixel.h>
 #define MAX_VAL 64  // 0 to 255 for brightness
 #define DELAY_TIME 50 
-#define DELAY_TIME2 20
+#define DELAY_TIME2 1
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
 
+bool ledon;
+//false:OFF, true:ON
+int ledstate;
+//0:Black, 1:Red, 2:Green, 3:Yellow, 4:Blue, 
+//5:Purple, 6:Cyan, 7:White, 8:rainbow, 9:rainboyCycle
+  
 void setup(){
+  ledon = false;
   // シリアルポートを9600 bps[ビット/秒]で初期化
   Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
 }
-
-int counter = 0;
- 
+  
 void loop(){
-   
   int input;
-  bool ledon;
+  
   // シリアルポートより1文字読み込む
   input = Serial.read();
-  
+
   if(input != -1 ){
     // 受け取った文字を送信
     Serial.write(input);
     Serial.write("\n");
-    if(ledon){
-      ledon = false;
+    
+    // 文字が来たらLEDの色を変更
+    /*
+    if(ledstate < 9){
+      ledstate++;
     }else{
-      ledon = true;
+      ledstate = 0;
     }
+    */
+    
+    // 文字が来たらLEDの色を変更
+    if(ledon == 0){
+      ledon = 1;
+    }else{
+      ledon = 0;
+    }
+    
   }
-  if(ledon && counter < 5){
-    colorWipe(strip.Color(MAX_VAL, 0, 0), DELAY_TIME); // Red
-    colorWipe(strip.Color(0, MAX_VAL, 0), DELAY_TIME); // Green
-    colorWipe(strip.Color(MAX_VAL, MAX_VAL, 0), DELAY_TIME); // Yellow
-    colorWipe(strip.Color(0, 0, MAX_VAL), DELAY_TIME); // Blue
-    colorWipe(strip.Color(MAX_VAL, 0, MAX_VAL), DELAY_TIME); // Purple
-    colorWipe(strip.Color(0, MAX_VAL, MAX_VAL), DELAY_TIME); // Cyan
-    colorWipe(strip.Color(MAX_VAL, MAX_VAL, MAX_VAL), DELAY_TIME); // White
-    rainbow(DELAY_TIME2);
-    rainbowCycle(DELAY_TIME2);
-    ledon = false;
-    counter++;
-  } else if(ledon) {
-    colorWipe(strip.Color(0, 0, 0), DELAY_TIME); //Black
+
+/*
+  switch(ledstate){
+    case:0
+        colorWipe(strip.Color(0, 0, 0), DELAY_TIME); //Black
+    break;
+    case:1
+        colorWipe(strip.Color(MAX_VAL, 0, 0), DELAY_TIME); // Red
+    break;
+    case:2
+        colorWipe(strip.Color(0, MAX_VAL, 0), DELAY_TIME); // Green
+    break;
+    case:3
+        colorWipe(strip.Color(MAX_VAL, MAX_VAL, 0), DELAY_TIME); // Yellow
+    break;
+    case:4
+        colorWipe(strip.Color(0, 0, MAX_VAL), DELAY_TIME); // Blue
+    break;
+    case:5
+        colorWipe(strip.Color(MAX_VAL, 0, MAX_VAL), DELAY_TIME); // Purple
+    break;
+    case:6
+        colorWipe(strip.Color(0, MAX_VAL, MAX_VAL), DELAY_TIME); // Cyan
+    break;
+    case:7
+        colorWipe(strip.Color(MAX_VAL, MAX_VAL, MAX_VAL), DELAY_TIME); // White
+    break;
+    case:8
+        rainbow(DELAY_TIME2);
+    break;
+    case:9
+        rainbowCycle(DELAY_TIME2);
+    break;
+  }
+  */
+  
+  if(ledon){
     //rainbow(DELAY_TIME2);
-    //rainbowCycle(DELAY_TIME2);
-    counter = 0;
-    ledon = false;
-  }  
+    rainbowCycle(DELAY_TIME2);
+    //rainbowCycle_blink(DELAY_TIME2);
+  }
+  else{
+    colorWipe(strip.Color(0, 0, 0), DELAY_TIME); //Black
+  }
+  
 }
 
  
@@ -77,15 +123,32 @@ void rainbow(uint8_t wait) {
 void rainbowCycle(uint8_t wait) {
   uint16_t i, j;
  
+  //for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+  for(j=0; j<256; j++) { // 1 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    //delay(wait);
+  }
+}
+
+void rainbowCycle_blink(uint8_t wait) {
+  uint16_t i, j;
+ 
   for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
     for(i=0; i< strip.numPixels(); i++) {
       strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
     }
     strip.show();
-    delay(wait);
+    //delay(wait);
+    //delay(0);
+    if(i%4 == 0){
+      colorWipe(strip.Color(0, 0, 0), 0); //Black
+    }
   }
 }
- 
+
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
 uint32_t Wheel(byte WheelPos) {
@@ -99,3 +162,4 @@ uint32_t Wheel(byte WheelPos) {
    return strip.Color(0, (WheelPos * 3)*MAX_VAL/255, (255 - WheelPos * 3)*MAX_VAL/255);
   }
 }
+
